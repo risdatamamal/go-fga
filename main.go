5ee79ffa2bad7b5de35755f31b20a65f7622c893
Risda Tamam Aljava <risda123tamam@student.esaunggul.ac.id>
2022-09-28 03:23:57 +0700

    hint subject

--- main.go
+++ main.go
@@ -1,4 +1,8 @@
 package main
 
 func main() {
+	// 1. init data student pada init function
+	// 2. program akan mengeluarkan pesan error, ketika id tidak ditemukan
+	// "student dengan id xx tidak ada pada database"
+	// 3. id harus positive integer
 }
